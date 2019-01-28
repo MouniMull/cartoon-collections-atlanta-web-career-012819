4e9ac8aef9674715639cd7e10f17239362ca0a76
Mouni Mullapudi <mullapudi24@gmail.com>
2019-01-28 01:40:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,13 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |dwarf, index|
-    puts "#{index + 1}. #{dwarf}"
-  end
+  dwarves.each_with_index{|dwarf, index| puts "#{index + 1}. #{dwarf}";} 
 end
-
 
 
 
 planeteer_calls = %w[earth wind fire water heart]
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map { |call| call.capitalize + '!' }
+ planeteer_calls.collect {|first_letter| puts "#{first_letter.capitalize}!."} 
 end
 
 summon_captain_planet(planeteer_calls)
